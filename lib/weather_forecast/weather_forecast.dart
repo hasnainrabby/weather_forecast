b28f400ed6c2c37f8ea1edcdb6a37c44c30a9235
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-10-26 21:00:20 +0600

    Using Future Builder to show data

--- lib/weather_forecast/weather_forecast.dart
+++ lib/weather_forecast/weather_forecast.dart
@@ -17,15 +17,57 @@ class _WeatherForecastState extends State<WeatherForecast> {
 
     super.initState();
     forecastObject = Network().getWeatherForecast(cityName: _cityName);
-    forecastObject.then((main){
-      print(main.coord);
-    });
+    // forecastObject.then((main){
+    //   print(main.coord);
+    // });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Weather Forecast"),
+      ),
+      body: ListView(
+        children: [
+           textFieldView(),
+          Container(
+            padding: EdgeInsets.only(top: 5.0),
+            child:
+            FutureBuilder
+              (
+              future: forecastObject,
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if(snapshot.hasData){
+                return Text("All Goods.");
+              }
+              else{
+                return Container(
+                  child: Center(child: CircularProgressIndicator(),),
+                );
+              }
+            },
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget textFieldView() {
+    return Container(
+      padding: EdgeInsets.only(top: 5.0,right: 5.0,left: 5.0),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(8),
+          hintText: "Enter City Name",
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),),
+        ),
+        onSubmitted: (value){
+
+        },
       ),
     );
   }
